5df5eb706de75b4cb2a6658a01713c8c23a8bd33
Ranieri Oliveira <114862419+ranierioliveira@users.noreply.github.com>
2023-05-01 11:42:53 -0300

    Diferentes maneiras de percorrer um arrays

---
+++ anotacoesRealizadosEmAula/percorrendoArray.js
@@ -0,0 +1,16 @@
+const myFriends1 = ["Sophia", "John", "Leif", "Winston", "OhNoes Guy", "Sal"];
+
+var friendNum = 0;
+while(friendNum < myFriends1.length) {
+    //console.log(myFriends1[friendNum]);
+    friendNum++;
+}
+
+
+for(pos of myFriends1){
+    //console.log(pos);
+}
+
+for(let c = 0; c < myFriends1.length; c++){
+    console.log(myFriends1[c]);
+}

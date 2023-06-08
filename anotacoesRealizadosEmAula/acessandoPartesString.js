52b39a759d4a8e7d1b937286152ed78eddce8cf5
Ranieri Oliveira <114862419+ranierioliveira@users.noreply.github.com>
2023-06-08 10:56:38 -0300

    Split, join, slice e alguns outros métodos para manipular strings

---
+++ anotacoesRealizadosEmAula/acessandoPartesString.js
@@ -0,0 +1,5 @@
+const str = 'Internet Explorer';
+const strQuebrada = str.split(' '); //remove os espaços e quebra a palavra em um vetor
+
+console.log(str.slice(0, 8));
+console.log(strQuebrada);
---
+++ anotacoesRealizadosEmAula/upperLower.js
@@ -0,0 +1,4 @@
+const str = 'Naquele dia frio de inverno, o aroma irresistível das batatas fritas recém-saídas do óleo quente enchia o ar, despertando em mim uma fome voraz.';
+
+const strLower = str.toLowerCase().replace('batata', 'manga');
+console.log(strLower);
